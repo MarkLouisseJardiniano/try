65abde367fc4d548c3e94b58910ab4352ae88917
Mark Louisse Jardiniano <112992310+MarkLouisseJardiniano@users.noreply.github.com>
2023-06-06 08:28:46 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
-
-
-
-function myFunction() {
-    var x = document.getElementById("food");
-    if (x.className === "food-filter") {
-      x.className += " fa";
-    } else {
-      x.className = "food-filter";
+// Function to handle the click event on the document
+document.addEventListener('click', function(event) {
+    var dropdown = document.querySelector('.food-type');
+    var dropdownContent = document.querySelector('.food-filter');
+    var targetElement = event.target;
+  
+    // Check if the clicked element is inside the dropdown or the dropdown button
+    if (!dropdown.contains(targetElement) && targetElement !== dropdown && targetElement !== dropdownContent) {
+      dropdownContent.classList.remove('active');
     }
+  });
+  
+  // Function to toggle the dropdown content
+  function myFunction() {
+    var dropdownContent = document.querySelector('.food-filter');
+    dropdownContent.classList.toggle('active');
   }
+  
+  // Add click event listener to each button inside the dropdown content
+  var buttons = document.querySelectorAll('.food-filter .buttons');
+  buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      var dropdownContent = document.querySelector('.food-filter');
+      dropdownContent.classList.remove('active');
+    });
+  });
+  
 
+
+
+
+
+
+
+  
 
 function orderpage(){
     document.location.href="menu.html";
